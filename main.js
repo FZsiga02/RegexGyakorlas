032d3383d1c5bf998022bb6025147fba09221493
FZsiga02 <fzsigmond02@gmail.com>
2022-10-27 12:14:20 +0200

    Form kész

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     let orszag = document.getElementById('orszag');
     let varos = document.getElementById('varos');
 
+    let utcaRegex = /[a-zA-z 0-9]{1,}/;
+    let utca = document.getElementById('utcaHazSzam');
+    let irSzam = document.getElementById('irSzam');
+
+    let kartyaRegex = /^[0-9]{4}-{1}[0-9]{4}-{1}[0-9]{4}-{1}[0-9]{4}$/;
+    let kSzam = document.getElementById('kartyaSzam');
+
+    let kodRegex = /^[0-9]{3}$/;
+    let kod = document.getElementById('kod');
+
+    let kNevRegex = /^[a-zA-Z ]{1,}$/;
+    let kNev = document.getElementById('kartyaNev');
+
     document.getElementById('fizetes').addEventListener('click', () => {
         if(!nevRegex.test(teljesNev.value)){
             document.getElementById('teljesNevKiir').textContent = "Legalább 1 betűnek kell lennie a névben";
@@ -20,10 +33,45 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         if(!nevRegex.test(varos.value)){
-            document.getElementById('orszagKiir').textContent = "Legalább 1 betűnek kell lennie az országban";
-            document.getElementById('orszagKiir').style.color = "red";
+            document.getElementById('varosKiir').textContent = "Legalább 1 betűnek kell lennie a városban";
+            document.getElementById('varosKiir').style.color = "red";
         } else{
-            document.getElementById('orszagKiir').textContent = "";
+            document.getElementById('varosKiir').textContent = "";
+        }
+
+        if(!utcaRegex.test(utca.value)){
+            document.getElementById('utcaKiir').textContent = "Legalább 1 betűnek kell lennie a lakcímben";
+            document.getElementById('utcaKiir').style.color = "red";
+        } else{
+            document.getElementById('utcaKiir').textContent = "";
+        }
+
+        if(!utcaRegex.test(irSzam.value)){
+            document.getElementById('irSzamKiir').textContent = "Legalább 1 karakternek kell lennie az irányítószámban";
+            document.getElementById('irSzamKiir').style.color = "red";
+        } else{
+            document.getElementById('irSzamKiir').textContent = "";
+        }
+
+        if(!kartyaRegex.test(kSzam.value)){
+            document.getElementById('kSzamKiir').textContent = "4x4 számjegy kell kötőjellel elválasztva";
+            document.getElementById('kSzamKiir').style.color = "red";
+        } else{
+            document.getElementById('kSzamKiir').textContent = "";
+        }
+
+        if(!kodRegex.test(kod.value)){
+            document.getElementById('kodKiir').textContent = "Pontosan 3 számjegy kell";
+            document.getElementById('kodKiir').style.color = "red";
+        } else{
+            document.getElementById('kodKiir').textContent = "";
+        }
+
+        if(!kNevRegex.test(kNev.value)){
+            document.getElementById('kNevKiir').textContent = "Angol abc betűi lehetnek";
+            document.getElementById('kNevKiir').style.color = "red";
+        } else{
+            document.getElementById('kNevKiir').textContent = "";
         }
     })
 })
